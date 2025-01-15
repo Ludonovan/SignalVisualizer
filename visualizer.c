cf59a414e6472354565923e32b23121411d13e8f
Ludonovan <lucasdonovan@live.com>
2025-01-14 21:33:21 -0500

    square wave plotting OK:

--- visualizer.c
+++ visualizer.c
@@ -11,9 +11,24 @@
 
 void error(char *msg);
 void generate_sine();
+void generate_square_wave();
 
 int main (int argc, char* argv[]) {
-    generate_sine();
+    int wv;
+	printf("Choose a waveform: \nSine (0) \nSquare (1)");
+	scanf("%d", &wv);
+	
+	switch(wv) {
+		case(0):
+			generate_sine();
+			break;
+		case(1):
+			generate_square_wave();
+			break;
+		default:
+			error("Error chosing waveform.");
+			break;
+	}
     return 0;
 }
 
@@ -23,9 +38,8 @@ int main (int argc, char* argv[]) {
 void generate_sine() {
     char *filename = "sine.csv";
     FILE *file = fopen(filename, "w");
-    if (!file) {
+    if (!file) 
         error("File failed to open.");
-    }
  
     double amp, freq, phase, duration, sampling_rate;
     printf("Amplitude: ");
@@ -47,6 +61,35 @@ void generate_sine() {
 
     fclose(file);
 }
+
+/*
+ * Generate a .csv file conaining a square wave
+ */
+void generate_square_wave() {
+	char *filename = "square.csv";
+	FILE *file = fopen(filename, "w");
+	if(!file)
+		error("File failed to open.");
+
+	double amp, freq, duration, sampling_rate;
+	printf("Amplitude: ");
+    scanf("%lf", &amp);
+    printf("Frequency: ");
+    scanf("%lf", &freq);
+    printf("Duration: ");
+    scanf("%lf", &duration);
+    printf("Sampling Rate: ");
+    scanf("%lf", &sampling_rate);
+	
+	fprintf(file, "time,value\n");
+
+	for (double t = 0; t <= duration; t += (1.0 / sampling_rate)) {
+		fprintf(file, "%lf,%lf\n", t, sin(2 * PI * freq * t) >= 0 ? amp : -amp);
+	}
+
+	fclose(file);
+}
+
 
 /*
  * Shortcut for printing to stderr
